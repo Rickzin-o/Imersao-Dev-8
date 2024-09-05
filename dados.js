eb0014bd935c560aa6e13ece4f60892a12406679
José Ricardo <ricardoanimation2@gmail.com>
2024-09-05 01:34:01 -0300

    add some examples to database

---
+++ dados.js
@@ -0,0 +1,34 @@
+let dados = [
+    {
+        "Hollow Knight": {
+            imagem: "https://cdn2.steamgriddb.com/grid/b4f32b14bdb9720ad4967fd0ddcaaa13.png",
+            desenvolvedor: "Team Cherry",
+            generos: "Plataforma, Metroidvania, Aventura",
+            resumo: "Hollow Knight é um metroidvania em 2D que te transporta para um reino subterrâneo em ruínas, habitado por criaturas bizarras e repleto de perigos. Com uma atmosfera sombria e melancólica, o jogo te convida a explorar cada canto de Hallownest, um reino outrora próspero, agora corroído pela infecção."
+        }
+    },
+    {
+        "Dead Cells": {
+            imagem: "https://cdn.cloudflare.steamstatic.com//steam/apps/588650/header_292x136.jpg",
+            desenvolvedor: "Motion Twin",
+            generos: "Roguelike, Plataforma, Ação",
+            resumo: "Dead Cells é um rogue-like de ação em 2D com combate frenético e progressão permanente. Explore um castelo em constante mutação, derrote inimigos grotescos e descubra novas habilidades a cada partida."
+        }
+    },
+    {
+        "Undertale": {
+            imagem: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/391540/header.jpg?t=1579096091",
+            desenvolvedor: "Toby Fox",
+            generos: "RPG, Aventura",
+            resumo: "Undertale é um RPG com uma história profunda e cheia de reviravoltas, onde suas escolhas influenciam o destino dos personagens. Com uma estética pixelada e uma trilha sonora memorável, Undertale oferece uma experiência única e emocionante."
+        }
+    },
+    {
+        "Stardew Valley": {
+            imagem: "https://shared.steamstatic.com/store_item_assets/steam/apps/413150/header.jpg?t=1711128146",
+            desenvolvedor: "ConcernedApe",
+            generos: "Simulação, RPG, Fazenda",
+            resumo: "Stardew Valley é um jogo de simulação de vida no campo onde você herda a fazenda de seu avô e precisa reconstruí-la. Cultive a terra, crie animais, explore minas e construa relacionamentos com os moradores da cidade."
+        }
+    }
+];
