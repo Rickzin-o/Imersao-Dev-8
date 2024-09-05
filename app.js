14587822e4ec824f3b8e9993f3e0cc0099dc17bf
José Ricardo <ricardoanimation2@gmail.com>
2024-09-05 01:44:38 -0300

    create loop to add games from database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var modal = document.querySelector("[data-modal]")
+var section_resultados = document.querySelector(".resultados-pesquisa")
 
 function openModal() {
     modal.showModal()
@@ -7,3 +8,9 @@ function openModal() {
 modal.addEventListener("click", () => {
     modal.close()
 })
+
+for (let i = 0; i < dados.length; i++) {
+    let divJogo = `<div class="item-resultado"><img src=${dados[i].imagem} onclick="openModal()"><p><strong>Gêneros: </strong>${dados[i].generos}</p></div>`
+
+    section_resultados.innerHTML += divJogo
+}
