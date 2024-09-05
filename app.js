c13bea0742e471d304657b962d10b06083ed5387
José Ricardo <ricardoanimation2@gmail.com>
2024-09-05 02:14:31 -0300

    create function to add data to modal

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var modal = document.querySelector("[data-modal]")
 var section_resultados = document.querySelector(".resultados-pesquisa")
 
-function openModal() {
-    modal.showModal()
+function openModal(jogo) {
+    dados.forEach(element => {
+            if (element.nome == jogo) {
+                let infosModal = `<img src=${element.imagem}"><p>${element.resumo}</p><p><strong>Desenvolvedor(es): </strong>${element.desenvolvedor}</p><p><strong>Gêneros: </strong>${element.generos}</p><button>Página da Steam</button>`
+                modal.innerHTML = infosModal
+                modal.showModal()
+        }
+    });
 }
 
 modal.addEventListener("click", () => {
@@ -10,7 +16,7 @@ modal.addEventListener("click", () => {
 })
 
 for (let i = 0; i < dados.length; i++) {
-    let divJogo = `<div class="item-resultado"><img src=${dados[i].imagem} onclick="openModal()"><p><strong>Gêneros: </strong>${dados[i].generos}</p></div>`
+    let divJogo = `<div class="item-resultado"><img src=${dados[i].imagem} onclick="openModal('${dados[i].nome}')"><p><strong>Gêneros: </strong>${dados[i].generos}</p></div>`
 
     section_resultados.innerHTML += divJogo
 }
